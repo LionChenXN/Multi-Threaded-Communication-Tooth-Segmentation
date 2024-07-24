31475dc41833faa617d6745fafe470e8b71da8ba
LionChenXN <98311497+LionChenXN@users.noreply.github.com>
2024-07-24 20:12:24 +0800

    client

---
+++ client/get_img.py
@@ -0,0 +1,28 @@
+import queue
+from utils.constants import *
+import os
+
+"""
+Author: Xinning Chen
+Finished Time: 24-7-2024
+Function: Load 2D tooth image from local
+"""
+
+# Load 2D tooth image from local
+def get_img(img_queue: queue.Queue):
+    path = DATA_PATH
+    if not os.path.exists(path):
+        os.makedirs(path)
+    names = os.listdir(path)
+
+    for name in names:
+        pic_path = path+f'/{name}'
+        img_queue.put(pic_path)
+
+
+
+if __name__ == '__main__':
+
+    img_queue = queue.Queue()
+    get_img(img_queue)
+
